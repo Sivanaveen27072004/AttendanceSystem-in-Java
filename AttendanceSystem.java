1739f513725135aba0fc8f593d1742ee6dbf8c6f
Sivnaveen L. <sivanaveenecemec@gmail.com>
2024-08-09 12:35:35 +0530

    Update AttendanceSystem.java

--- AttendanceSystem.java
+++ AttendanceSystem.java
@@ -3,6 +3,9 @@ class Student
 {
 void newattandance()
 {
+System.out.println();
+System.out.println("----------------You Are in Student Attendance--------------------");
+System.out.println();
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter your Roll.no");
 int opt=sc.nextInt();
@@ -10,79 +13,275 @@ switch(opt)
 {
 case 132:
           System.out.println();
-          System.out.println("****************************************");
+          System.out.println("************** Welcome "+opt+" **************************");
           System.out.println();
           int count=0;
           System.out.println("Choose below Option");
           System.out.println("1.Enter Today Attantance");
-          System.out.println("2.Check The Total Days Present");
           int choise=sc.nextInt();
           if(choise==1)
-          {
-           System.out.println(count);
-           System.out.println("Put 1 to today Attanence ");
-           int today=sc.nextInt();
-           count=count+1;
-           System.out.println(count);
-          
+         {
+           System.out.println("Put 1 to Today Attendance");
+           int today = sc.nextInt();
+           if(today==1) count++;
+           System.out.println(" put 1 to check total% and put 2to exit");
+           int chk=sc.nextInt();
+           if(chk==1) System.out.println("The total Count is :"+count);
+           else System.out.println("Thank you!!!!!");
+          }
+          else
+           System.out.print("Thank You !!!!!");
            break;
-           }
-           else if(choise==2)
-           {
-            System.out.println(count);
-            break;
-            }
-            else
-           System.out.println("Enter the valid option");
-           
 case 117: 
-          System.out.println();
-          System.out.println("****************************************");
+          System.out.println();          
+          System.out.println("************** Welcome "+opt+" **************************");
           System.out.println();
           int count1=0;
           System.out.println("Choose below Option");
           System.out.println("1.Enter Today Attantance");
-          System.out.println("2.Check The Total Days Present");
           int choise1=sc.nextInt();
           if(choise1==1)
-          {
-           System.out.println("Put 1 to today Attanence ");
-           int today1=sc.nextInt();
-           count1++;
+         {
+           System.out.println("Put 1 to Today Attendance");
+           int today1 = sc.nextInt();
+           if(today1==1) count1++;
+           System.out.println(" put 1 to check total% and put 2to exit");
+           int chk1=sc.nextInt();
+           if(chk1==1) System.out.println("The total Count is :"+count1);
+           else System.out.println("Thank you!!!!!");
+          }
+          else
+           System.out.print("Thank You !!!!!");
            break;
-           }
-           else if(choise1==2)
-           {
-            System.out.println(count1);
-            break;
-            }
-            else
-           System.out.println("Enter the valid option");
-           
+case 147:
+          System.out.println();
+          System.out.println("************** Welcome "+opt+" **************************");
+          System.out.println();
+          int count2=0;
+          System.out.println("Choose below Option");
+          System.out.println("1.Enter Today Attantance");
+          int choise2=sc.nextInt();
+          if(choise2==1)
+         {
+           System.out.println("Put 1 to Today Attendance");
+           int today2= sc.nextInt();
+           if(today2==1) count2++;
+           System.out.println(" put 1 to check total% and put 2to exit");
+           int chk2=sc.nextInt();
+           if(chk2==1) System.out.println("The total Count is :"+count2);
+           else System.out.println("Thank you!!!!!");
+          }
+          else
+           System.out.print("Thank You !!!!!");
+           break;
 default:
           System.out.println("Enter your RollNo properly");
-}
-}
+         }
+     }
 }
 class Staff
 {
-void display(){
-System.out.println("you are in Staff");
-}
+void newattandance()
+{
+System.out.println();
+System.out.println("----------------You Are in Staff Attendance--------------------");
+System.out.println();
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your Id no");
+int opts=sc.nextInt();
+switch(opts)
+{
+case 1000:
+          System.out.println();
+          System.out.println("************** Welcome "+opts+" **************************");
+          System.out.println();
+          int counts=0;
+          System.out.println("Choose below Option");
+          System.out.println("1.Enter Today Attantance");
+          int choises=sc.nextInt();
+          if(choises==1)
+         {
+           System.out.println("Put 1 to Today Attendance");
+           int todays = sc.nextInt();
+           if(todays==1) counts++;
+           System.out.println(" put 1 to check total% and put 2to exit");
+           int chks=sc.nextInt();
+           if(chks==1) System.out.println("The total Count is :"+counts);
+           else System.out.println("Thank you!!!!!");
+          }
+          else
+           System.out.print("Thank You !!!!!");
+           break;
+case 1500: 
+          System.out.println();
+          System.out.println("************** Welcome "+opts+" **************************");
+          System.out.println();
+          int count1s=0;
+          System.out.println("Choose below Option");
+          System.out.println("1.Enter Today Attantance");
+          int choise1s=sc.nextInt();
+          if(choise1s==1)
+         {
+           System.out.println("Put 1 to Today Attendance");
+           int today1s = sc.nextInt();
+           if(today1s==1) count1s++;
+           System.out.println(" put 1 to check total% and put 2to exit");
+           int chk1s=sc.nextInt();
+           if(chk1s==1) System.out.println("The total Count is :"+count1s);
+           else System.out.println("Thank you!!!!!");
+          }
+          else
+           System.out.print("Thank You !!!!!");
+           break;
+case 2000:
+          System.out.println();
+          System.out.println("************** Welcome "+opts+" **************************");
+          System.out.println();
+          int count2s=0;
+          System.out.println("Choose below Option");
+          System.out.println("1.Enter Today Attantance");
+          int choise2s=sc.nextInt();
+          if(choise2s==1)
+         {
+           System.out.println("Put 1 to Today Attendance");
+           int today2s= sc.nextInt();
+           if(today2s==1) count2s++;
+           System.out.println(" put 1 to check total% and put 2to exit");
+           int chk2s=sc.nextInt();
+           if(chk2s==1) System.out.println("The total Count is :"+count2s);
+           else System.out.println("Thank you!!!!!");
+          }
+          else
+           System.out.print("Thank You !!!!!");
+           break;
+default:
+          System.out.println("Enter your Id No properly");
+         }
+     }
+
 }
 class Hod
 {
-void display(){
-System.out.println("You are in Hod");
-}
+void newattandance()
+{
+System.out.println();
+System.out.println("----------------You Are in HoD's Attendance--------------------");
+System.out.println();
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your Id no");
+int opth=sc.nextInt();
+switch(opth)
+{
+case 10:
+          System.out.println();
+          System.out.println("************** Welcome "+opth+" **************************");
+          System.out.println();
+          int counth=0;
+          System.out.println("Choose below Option");
+          System.out.println("1.Enter Today Attantance");
+          int choiseh=sc.nextInt();
+          if(choiseh==1)
+         {
+           System.out.println("Put 1 to Today Attendance");
+           int todayh = sc.nextInt();
+           if(todayh==1) counth++;
+           System.out.println(" put 1 to check total% and put 2to exit");
+           int chkh=sc.nextInt();
+           if(chkh==1) System.out.println("The total Count is :"+counth);
+           else System.out.println("Thank you!!!!!");
+          }
+          else
+           System.out.print("Thank You !!!!!");
+           break;
+case 15: 
+          System.out.println();
+          System.out.println("************** Welcome "+opth+" **************************");
+          System.out.println();
+          int count1h=0;
+          System.out.println("Choose below Option");
+          System.out.println("1.Enter Today Attantance");
+          int choise1h=sc.nextInt();
+          if(choise1h==1)
+         {
+           System.out.println("Put 1 to Today Attendance");
+           int today1h = sc.nextInt();
+           if(today1h==1) count1h++;
+           System.out.println(" put 1 to check total% and put 2to exit");
+           int chk1h=sc.nextInt();
+           if(chk1h==1) System.out.println("The total Count is :"+count1h);
+           else System.out.println("Thank you!!!!!");
+          }
+          else
+           System.out.print("Thank You !!!!!");
+           break;
+case 20:
+          System.out.println();
+          System.out.println("************** Welcome "+opth+" **************************");
+          System.out.println();
+          int count2h=0;
+          System.out.println("Choose below Option");
+          System.out.println("1.Enter Today Attantance");
+          int choise2h=sc.nextInt();
+          if(choise2h==1)
+         {
+           System.out.println("Put 1 to Today Attendance");
+           int today2h= sc.nextInt();
+           if(today2h==1) count2h++;
+           System.out.println(" put 1 to check total% and put 2to exit");
+           int chk2h=sc.nextInt();
+           if(chk2h==1) System.out.println("The total Count is :"+count2h);
+           else System.out.println("Thank you!!!!!");
+          }
+          else
+           System.out.print("Thank You !!!!!");
+           break;
+default:
+          System.out.println("Enter your Id no properly");
+         }
+     }
+
+
+
 }
 class Principal
 {
-void display(){
-System.out.println("you are in Admin");
+void newattandance()
+{
+System.out.println();
+System.out.println("----------------You Are in Principal  Attendance--------------------");
+System.out.println();
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter your Password");
+int opt=sc.nextInt();
+switch(opt)
+{
+case 27072004:
+          System.out.println();
+          System.out.println("****************************************");
+          System.out.println();
+          int countp=0;
+          System.out.println("Choose below Option");
+          System.out.println("1.Enter Today Attantance");
+          int choisep=sc.nextInt();
+          if(choisep==1)
+         {
+           System.out.println("Put 1 to Today Attendance");
+           int todayp= sc.nextInt();
+           if(todayp==1) countp++;
+           System.out.println(" put 1 to check total% and put 2to exit");
+           int chkp=sc.nextInt();
+           if(chkp==1) System.out.println("The total Count is :"+countp);
+           else System.out.println("Thank you!!!!!");
+          }
+          else
+           System.out.print("Thank You !!!!!");
+           break;
+default:
+          System.out.println("Password Incorrect");
+        }
+     }
 }
-}
-public class AttanenceDeriver
+public class AttendanceDriverFinal
 {
 public static void main(String [] args)
 {
@@ -96,23 +295,27 @@ System.out.println();
 System.out.println("****************************************");
 System.out.println();
 System.out.println("-------------Welcome to Attandance System-------------");
+System.out.println();
 System.out.println("1.Principal");
 System.out.println("2.HoD");
 System.out.println("3.Staff");
 System.out.println("4.Student");
+System.out.println();
+System.out.println("******************************************");
+System.out.println();
 Scanner sc=new Scanner(System.in);
 System.out.println("Enter your Option");
 int Opt=sc.nextInt();
 switch(Opt)
 {
 case 1:
-p.display();
+p.newattandance();
 break;
 case 2:
-h.display();
+h.newattandance();
 break;
 case 3:
-t.display();
+t.newattandance();
 break;
 case 4:
 s.newattandance();
@@ -120,6 +323,23 @@ break;
 default:
 System.out.println("Enter the valid number");     
 }
+System.out.println();
+System.out.println("*****************************************");
+System.out.println();
+System.out.println("With Regards,");
+System.out.println("                      Sivanaveen Lakshmanasamy");
+System.out.println("                      Admin Of Attendance System");
+System.out.println("                      Student of  ECE");
+System.out.println("                      2021-2025 Batch");
+System.out.println("                      Mahendra Engineering College");
+System.out.println("                      Namakkal.");
+System.out.println();
+System.out.println("                                 Thank you                 ");
+System.out.println();
+System.out.println("********************************************");
+System.out.println();
+System.out.println();
+
 }
 }
 }
